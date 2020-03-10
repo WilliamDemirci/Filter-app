0732c572e2c00fc37a87f0f1281b07890e104ee0
WilliamDemirci <wil.8dev@gmail.com>
2020-03-10 11:14:33 +0100

    add progress bar with location permission check / internet access check

--- app/src/main/java/com/linkvalue/filtrecovid_19/MainActivity.kt
+++ app/src/main/java/com/linkvalue/filtrecovid_19/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.linkvalue.filtrecovid_19
 
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,23 +43,45 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        setDisabledButton() // by default, filter is disabled
+        disabledButtonTemplate() // by default, filter is disabled
+        buttonState = false
         Log.i("MainActivity", "Initialised as disabled")
     }
 
     private fun setDisabledButton() {
-        disabledButtonTemplate()
+        progressBarDisconnection.visibility = View.VISIBLE
+        Handler().postDelayed(
+            {
+                progressBarDisconnection.visibility = View.INVISIBLE
+                disabledButtonTemplate()
+            },
+            1500
+        )
         buttonState = false
+
         Log.i("MainActivity", "Filter disabled")
     }
 
     private fun setActivatedButton() {
-        if (permissionGranted()) {
-            activatedButtonTemplate()
+        if (permissionGranted() && isInternetConnection()) {
+
+            progressBarConnection.visibility = View.VISIBLE
+            Handler().postDelayed(
+                {
+                    progressBarConnection.visibility = View.GONE
+                    activatedButtonTemplate()
+                },
+                3000
+            )
+
             buttonState = true
             Log.i("MainActivity", "Filter activated")
 
             activationCounter() // count the number of activation to propose the Premium version
+        } else if (!permissionGranted()) {
+            locationSnackbar()
+        } else if (!isInternetConnection()) {
+            internetConnectionSnackbar()
         }
     }
 
@@ -113,9 +141,46 @@ class MainActivity : AppCompatActivity() {
             Snackbar.LENGTH_INDEFINITE
         )
         snackbar.setAction(getString(R.string.close_snackbar_text)) {
-            Log.i("MainActivity", "Clicked on Snackbar")
+            Log.i("MainActivity", "Clicked on Snackbar : showPremiumSnackbar")
         }
         snackbar.setActionTextColor(Color.WHITE)
         snackbar.show()
+        Log.i("MainActivity", "Show Snackbar : showPremiumSnackbar")
+    }
+
+    private fun internetConnectionSnackbar() {
+        val snackbarNetwork = Snackbar.make(
+            rootLayout,
+            getString(R.string.internet_connection_error_snackbar_text),
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snackbarNetwork.setAction(getString(R.string.close_snackbar_text)) {
+            Log.i("MainActivity", "Clicked on Snackbar : internetConnectionSnackbar")
+        }
+        snackbarNetwork.setActionTextColor(Color.WHITE)
+        snackbarNetwork.show()
+        Log.i("MainActivity", "Show Snackbar : internetConnectionSnackbar")
+    }
+
+    private fun locationSnackbar() {
+        val snackbarNetwork = Snackbar.make(
+            rootLayout,
+            getString(R.string.location_error_snackbar_text),
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snackbarNetwork.setAction(getString(R.string.close_snackbar_text)) {
+            Log.i("MainActivity", "Clicked on Snackbar : locationSnackbar")
+        }
+        snackbarNetwork.setActionTextColor(Color.WHITE)
+        snackbarNetwork.show()
+        Log.i("MainActivity", "Show Snackbar : locationSnackbar")
+    }
+
+    private fun isInternetConnection(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capability =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return capability?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
     }
 }
